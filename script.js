88d3f5fcd61e0e2314688dfb47e2daa3a5f8fed7
Domagojsk <domagoj.bane@gmail.com>
2022-03-18 15:29:39 +0100

    javascript added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let searchBtn = document.querySelector('.search-btn');
+let searchInput = document.querySelector('.search-input');
+
+/* using Enter key */
+searchInput.addEventListener('keydown', function (event) {
+  if (event.keyCode === 13) {
+    let url = 'https://www.google.hr/search?q=' + searchInput.value;
+    window.open(url, '_self');
+    searchInput.value = '';
+  }
+});
+
+/* Button click */
+searchBtn.onclick = function () {
+  let url = 'https://www.google.hr/search?q=' + searchInput.value;
+  window.open(url, '_self');
+  searchInput.value = '';
+};
